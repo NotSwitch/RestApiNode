ed7462cdfd3f932b7e7fd36d7fbc500f58492262
Vishal Kumar Tyagi <vtyagi0097@gmail.com>
2022-08-22 22:49:53 +0530

    vishal

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("./db/conn")
 
-const studentRouter = require("./routers/studentrouter");
+//const studentRouter = require("./routers/studentrouter");
 const teacherRouter = require("./routers/teacherrouter");
 const coursesRouter = require("./routers/coursesrouter");
 const uploadApi = require("./routers/imageupload");
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(studentRouter);
+//app.use(studentRouter);
 app.use(teacherRouter);
 app.use(uploadApi);
 app.use(coursesRouter);
