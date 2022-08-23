e124a138b6d6a399bec2b814b35a32f468c7a55f
Vishal Kumar Tyagi <vtyagi0097@gmail.com>
2022-08-24 01:03:24 +0530

    change path

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://vishaltyagi007:UFVinjGQnvI3I3aN@cluster0.zpzkhll.mongodb.net/newdb",{useNewUrlParser: true,useUnifiedTopology: true}).then(() => console.log("Connect Successfully")).catch((err) => console.log(err));
+mongoose.connect("mongodb+srv://vishaltyagi007:UFVinjGQnvI3I3aN@cluster0.zpzkhll.mongodb.net/test",{useNewUrlParser: true,useUnifiedTopology: true}).then(() => console.log("Connect Successfully")).catch((err) => console.log(err));
 
 /* mongodb://192.168.1.58:27017/students-api8  -- localhost connectionString */
