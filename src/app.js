7591e8278a7c8b5bbaede2812bb409dcdacc1852
Vishal Kumar Tyagi <vtyagi0097@gmail.com>
2022-08-24 20:06:57 +0530

    change path

--- src/app.js
+++ src/app.js
@@ -9,6 +9,13 @@ const uploadApi = require("./routers/imageupload");
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers",
+             "Origin, X-Requested-With,Content-Type,Accept,Authorization");
+  res.header('Access-Control-Allow-Methods','GET,PUT,PATCH,POST,DELETE,OPTIONS');
+  next();           
+})
 app.use(express.json());
 //app.use(studentRouter);
 app.use(teacherRouter);
